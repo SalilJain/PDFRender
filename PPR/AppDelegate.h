afb71fcada1388e80d2f13d49a6d21fa4c66d77f
Salil Jain <saliljain@Salils-MacBook-Pro.local>
2012-06-04 13:03:27 -0400

    Initial Commit

---
+++ PPR/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PPR
+//
+//  Created by Salil Jain on 6/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PPR/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PPR
+//
+//  Created by Salil Jain on 6/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
